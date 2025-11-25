681b620d7609b561e0dc76d2074f6604ceaa6cd9
mrhorst <mhorst31@gmail.com>
2025-11-25 13:19:01 -0500

    implement very simple user profile page

--- mobile/app/screens/ProfileScreen.tsx
+++ mobile/app/screens/ProfileScreen.tsx
@@ -3,17 +3,37 @@ import { useNavigate } from 'react-router-native'
 import { useLoggedUser } from '../context/user/UserContext'
 import { useAuth } from '../context/auth/AuthContext'
 import styles from './styles'
+import { Stack } from 'expo-router'
 
 const ProfileScreen = () => {
   const nav = useNavigate()
   const { user } = useLoggedUser()
   const { logout } = useAuth()
+
+  if (!user) return null
+
+  const date = new Date(user.iat * 1000).toLocaleString()
+
   return (
     <View style={styles.screenContainer}>
+      <Stack.Screen options={{ title: 'Profile' }} />
       <View style={styles.headerContainer}>
         <Button title='Back' onPress={() => nav(-1)}></Button>
-        <Text style={{ fontSize: 18, fontWeight: '700' }}>{user?.name}</Text>
         <Button title='Logout' onPress={logout} />
+      </View>
+      <View style={{ marginTop: 50, gap: 30 }}>
+        <View style={{ flexDirection: 'row', gap: 30 }}>
+          <Text style={{ fontWeight: 600, flex: 1 }}>Name:</Text>
+          <Text>{user.name}</Text>
+        </View>
+        <View style={{ flexDirection: 'row', gap: 30 }}>
+          <Text style={{ fontWeight: 600, flex: 1 }}>Email:</Text>
+          <Text>{user.email}</Text>
+        </View>
+        <View style={{ flexDirection: 'row', gap: 30 }}>
+          <Text style={{ fontWeight: 600, flex: 1 }}>Last login:</Text>
+          <Text>{date}</Text>
+        </View>
       </View>
     </View>
   )
