e41c30f7d13700f02f43ee080888223cd04451e1
mrhorst <mhorst31@gmail.com>
2025-12-08 23:59:44 -0500

    refactor long code into 2 helper functions: `getSectionMembers` and `getSectionNonMembers`

--- backend/src/controllers/sections.ts
+++ backend/src/controllers/sections.ts
@@ -1,49 +1,72 @@
-import { Request, Response } from 'express'
-import { section, sectionMember, user } from '@/db/schema.ts'
+import { NextFunction, Request, Response } from 'express'
+import { section, sectionMember } from '@/db/schema.ts'
 import db from '@/utils/db.ts'
-import { and, eq, notExists } from 'drizzle-orm'
-import { getUserSectionRole } from '@/utils/controllerHelpers.ts'
+import { and, eq } from 'drizzle-orm'
+import {
+  getSectionMembers,
+  getSectionNonMembers,
+  getUserSectionRole,
+} from '@/utils/controllerHelpers.ts'
 import { AuthenticatedRequest } from '@/types/index.ts'
 
-const listSections = async (req: AuthenticatedRequest, res: Response) => {
+const listSections = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const userId = Number(req.user!.sub)
+  try {
+    const subscribed = await db
+      .select({
+        id: section.id,
+        name: section.name,
+        createdAt: section.createdAt,
+        role: sectionMember.role,
+      })
+      .from(sectionMember)
+      .where(and(eq(sectionMember.userId, userId), eq(section.active, true)))
+      .leftJoin(section, eq(sectionMember.sectionId, section.id))
 
-  const subscribed = await db
-    .select({
-      id: section.id,
-      name: section.name,
-      createdAt: section.createdAt,
-      role: sectionMember.role,
-    })
-    .from(sectionMember)
-    .where(and(eq(sectionMember.userId, userId), eq(section.active, true)))
-    .leftJoin(section, eq(sectionMember.sectionId, section.id))
-
-  res.status(200).send(subscribed)
+    res.status(200).send(subscribed)
+  } catch (error) {
+    next(error)
+  }
 }
 
-const createSection = async (req: AuthenticatedRequest, res: Response) => {
+const createSection = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const { name } = req.body
   const ownerId = Number(req.user!.sub)
   const newSection = { name, ownerId }
 
-  const [addedSection, addedSectionMember] = await db.transaction(
-    async (tx) => {
-      const [addedSection] = await tx
-        .insert(section)
-        .values(newSection)
-        .returning()
+  try {
+    const [addedSection, addedSectionMember] = await db.transaction(
+      async (tx) => {
+        const [addedSection] = await tx
+          .insert(section)
+          .values(newSection)
+          .returning()
 
-      const [addedSectionMember] = await tx
-        .insert(sectionMember)
-        .values({ userId: ownerId, sectionId: addedSection.id, role: 'owner' })
-        .returning()
+        const [addedSectionMember] = await tx
+          .insert(sectionMember)
+          .values({
+            userId: ownerId,
+            sectionId: addedSection.id,
+            role: 'owner',
+          })
+          .returning()
 
-      return [addedSection, addedSectionMember] as const
-    }
-  )
+        return [addedSection, addedSectionMember] as const
+      }
+    )
 
-  res.status(201).send({ addedSection, addedSectionMember })
+    res.status(201).send({ addedSection, addedSectionMember })
+  } catch (error) {
+    next(error)
+  }
 }
 const updateSection = (_req: Request, res: Response) => {
   res.status(501).send('TODO')
@@ -52,125 +75,113 @@ const deleteSection = (_req: Request, res: Response) => {
   res.status(501).send('TODO')
 }
 
-const getSectionInfo = async (req: AuthenticatedRequest, res: Response) => {
+const getSectionInfo = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const sectionId = Number(req.params.id)
 
   const loggedUser = Number(req.user?.sub)
 
-  const requesterRole = await getUserSectionRole(loggedUser, sectionId)
-  if (requesterRole !== 'owner') {
-    return res
-      .status(403)
-      .send({ error: 'only owners can see the section info' })
+  try {
+    const requesterRole = await getUserSectionRole(loggedUser, sectionId)
+    if (requesterRole !== 'owner') {
+      return res
+        .status(403)
+        .send({ error: 'only owners can see the section info' })
+    }
+
+    const members = await getSectionMembers(sectionId)
+    const nonMembers = await getSectionNonMembers(sectionId)
+
+    res.send({ members, nonMembers })
+  } catch (error) {
+    next(error)
   }
-
-  const members = await db
-    .select({
-      name: user.name,
-      sectionName: section.name,
-      userId: user.id,
-      role: sectionMember.role,
-      sectionId: sectionMember.sectionId,
-    })
-    .from(sectionMember)
-    .where(
-      and(eq(sectionMember.sectionId, sectionId), eq(section.active, true))
-    )
-    .leftJoin(user, eq(sectionMember.userId, user.id))
-    .leftJoin(section, eq(section.id, sectionId))
-
-  const nonMembers = await db
-    .select({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-    })
-    .from(user)
-    .where(
-      and(
-        eq(user.active, true),
-        notExists(
-          db
-            .select()
-            .from(sectionMember)
-            .where(
-              and(
-                eq(sectionMember.userId, user.id),
-                eq(sectionMember.sectionId, sectionId)
-              )
-            )
-        )
-      )
-    )
-
-  res.send({ members, nonMembers })
 }
 
-const addMember = async (req: AuthenticatedRequest, res: Response) => {
+const addMember = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const { sectionId, userId } = req.body
   const loggedUser = Number(req.user?.sub)
+  try {
+    const requesterRole = await getUserSectionRole(loggedUser, sectionId)
+    if (requesterRole !== 'owner') {
+      return res.status(403).send({ error: 'only owners can add members' })
+    }
 
-  const requesterRole = await getUserSectionRole(loggedUser, sectionId)
-  if (requesterRole !== 'owner') {
-    return res.status(403).send({ error: 'only owners can add members' })
+    await db.insert(sectionMember).values({ sectionId, userId, role: 'viewer' })
+    res.send(201)
+  } catch (error) {
+    next(error)
   }
-
-  await db.insert(sectionMember).values({ sectionId, userId, role: 'viewer' })
-  res.send(201)
 }
 
-const updateMemberRole = async (req: AuthenticatedRequest, res: Response) => {
+const updateMemberRole = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const { sectionId, userId, role } = req.body
   const loggedUser = Number(req.user?.sub)
+  try {
+    const requesterRole = await getUserSectionRole(loggedUser, sectionId)
+    if (requesterRole !== 'owner') {
+      return res
+        .status(403)
+        .send({ error: 'only owners can update member roles' })
+    }
 
-  const requesterRole = await getUserSectionRole(loggedUser, sectionId)
-  if (requesterRole !== 'owner') {
-    return res
-      .status(403)
-      .send({ error: 'only owners can update member roles' })
-  }
-
-  const updatedMember = await db
-    .update(sectionMember)
-    .set({ role })
-    .where(
-      and(
-        eq(sectionMember.sectionId, sectionId),
-        eq(sectionMember.userId, userId)
+    const updatedMember = await db
+      .update(sectionMember)
+      .set({ role })
+      .where(
+        and(
+          eq(sectionMember.sectionId, sectionId),
+          eq(sectionMember.userId, userId)
+        )
       )
-    )
-    .returning()
-  res.send(updatedMember)
+      .returning()
+    res.send(updatedMember)
+  } catch (error) {
+    next(error)
+  }
 }
 
-const unsubscribeMember = async (req: AuthenticatedRequest, res: Response) => {
+const unsubscribeMember = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
   const { id: sectionId, userId } = req.params
-  console.log(req.params)
-
-  const requesterRole = await getUserSectionRole(
-    Number(req.user?.sub),
-    Number(sectionId)
-  )
-
-  console.log('section id: ', sectionId)
-  console.log('user id: ', req.user?.sub)
-  console.log('requester role: ', requesterRole)
-
-  if (requesterRole !== 'owner') {
-    return res
-      .status(403)
-      .send({ error: 'only owners can unsubscribe members.' })
-  }
-
-  await db
-    .delete(sectionMember)
-    .where(
-      and(
-        eq(sectionMember.sectionId, Number(sectionId)),
-        eq(sectionMember.userId, Number(userId))
-      )
+  try {
+    const requesterRole = await getUserSectionRole(
+      Number(req.user?.sub),
+      Number(sectionId)
     )
-  res.send(204)
+
+    if (requesterRole !== 'owner') {
+      return res
+        .status(403)
+        .send({ error: 'only owners can unsubscribe members.' })
+    }
+
+    await db
+      .delete(sectionMember)
+      .where(
+        and(
+          eq(sectionMember.sectionId, Number(sectionId)),
+          eq(sectionMember.userId, Number(userId))
+        )
+      )
+    res.send(204)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export {
--- backend/src/utils/controllerHelpers.ts
+++ backend/src/utils/controllerHelpers.ts
@@ -1,6 +1,6 @@
-import { sectionMember } from '@/db/schema.ts'
+import { section, sectionMember, user } from '@/db/schema.ts'
 import db from './db.ts'
-import { and, eq } from 'drizzle-orm'
+import { and, eq, notExists } from 'drizzle-orm'
 
 export const getUserSectionRole = async (userId: number, sectionId: number) => {
   const result = await db
@@ -15,4 +15,50 @@ export const getUserSectionRole = async (userId: number, sectionId: number) => {
     .limit(1)
 
   return result[0]?.role || null
+}
+
+export const getSectionNonMembers = async (sectionId: number) => {
+  const nonMembers = await db
+    .select({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    })
+    .from(user)
+    .where(
+      and(
+        eq(user.active, true),
+        notExists(
+          db
+            .select()
+            .from(sectionMember)
+            .where(
+              and(
+                eq(sectionMember.userId, user.id),
+                eq(sectionMember.sectionId, sectionId)
+              )
+            )
+        )
+      )
+    )
+  return nonMembers
+}
+
+export const getSectionMembers = async (sectionId: number) => {
+  const members = await db
+    .select({
+      name: user.name,
+      sectionName: section.name,
+      userId: user.id,
+      role: sectionMember.role,
+      sectionId: sectionMember.sectionId,
+    })
+    .from(sectionMember)
+    .where(
+      and(eq(sectionMember.sectionId, sectionId), eq(section.active, true))
+    )
+    .leftJoin(user, eq(sectionMember.userId, user.id))
+    .leftJoin(section, eq(section.id, sectionId))
+
+  return members
 }
