270762b8d2c35b67cbe930e48f0b8ad0aa6873f2
mrhorst <mhorst31@gmail.com>
2025-12-16 15:21:49 -0500

    add icons to tabbar

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,18 +1,63 @@
+import { Ionicons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name='dashboard' options={{ title: 'Dashboard' }} />
+      <Tabs.Screen
+        name='dashboard'
+        options={{
+          title: 'Dashboard',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
         name='tasks'
-        options={{ headerShown: false, title: 'Tasks' }}
+        options={{
+          headerShown: false,
+          title: 'Tasks',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'checkmark-done' : 'checkmark-done-outline'}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='sections'
-        options={{ headerShown: false, title: 'Sections' }}
+        options={{
+          headerShown: false,
+          title: 'Sections',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'layers' : 'layers-outline'}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
       />
-      <Tabs.Screen name='user' options={{ title: 'User' }} />
+      <Tabs.Screen
+        name='user'
+        options={{
+          title: 'User',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? 'person' : 'person-outline'}
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   )
 }
